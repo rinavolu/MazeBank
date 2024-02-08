5eb77cb0e74170de05eb6aa9486f760f3307c0cc
Ravindranadh Inavolu <ravindranadh.inavolu@gmail.com>
2024-02-08 10:52:33 +0530

    Created Login Form

--- src/main/java/com/bank/maze/mazebank/BankApplication.java
+++ src/main/java/com/bank/maze/mazebank/BankApplication.java
@@ -1,6 +1,18 @@
 package com.bank.maze.mazebank;
 
-public class BankApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BankApplication extends Application {
 
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+        Scene scene =new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/com/bank/maze/mazebank/Controllers/LoginController.java
+++ src/main/java/com/bank/maze/mazebank/Controllers/LoginController.java
@@ -1,4 +1,14 @@
 package com.bank.maze.mazebank.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox account_selector;
+    public TextField payee_address_fld;
+    public TextField password_fld;
+    public Label error_lbl;
+    public Button login_btn;
 }
