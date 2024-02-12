74db1de56633ff59fcb7d6340a8f493b282d9b75
Ravindranadh Inavolu <ravindranadh.inavolu@gmail.com>
2024-02-12 13:14:46 +0530

    Created Client left Pane

---
+++ src/main/java/com/bank/maze/mazebank/Controllers/Client/ClientMenuController.java
@@ -0,0 +1,12 @@
+package com.bank.maze.mazebank.Controllers.Client;
+
+import javafx.scene.control.Button;
+
+public class ClientMenuController {
+    public Button dashboard_btn;
+    public Button transaction_btn;
+    public Button account_btn;
+    public Button profile_btn;
+    public Button logout_btn;
+    public Button report_btn;
+}
--- src/main/java/com/bank/maze/mazebank/Controllers/LoginController.java
+++ src/main/java/com/bank/maze/mazebank/Controllers/LoginController.java
@@ -1,14 +1,23 @@
 package com.bank.maze.mazebank.Controllers;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     public ChoiceBox account_selector;
     public TextField payee_address_fld;
     public TextField password_fld;
     public Label error_lbl;
     public Button login_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
