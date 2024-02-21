23b03a2385be0300f9b67d540ee13527fd9fc78d
Ravindranadh Inavolu <ravindranadh.inavolu@gmail.com>
2024-02-21 17:45:40 +0530

    Created Client Transaction Cell

--- src/main/java/com/bank/maze/mazebank/Controllers/Client/TransactionsController.java
+++ src/main/java/com/bank/maze/mazebank/Controllers/Client/TransactionsController.java
@@ -1,6 +1,8 @@
 package com.bank.maze.mazebank.Controllers.Client;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
@@ -8,6 +10,12 @@ import java.util.ResourceBundle;
 
 public class TransactionsController implements Initializable {
     public ListView transactions_list_view;
+    public FontAwesomeIconView transaction_credit_icon;
+    public FontAwesomeIconView transaction_debit_icon;
+    public Label transaction_date;
+    public Label sender;
+    public Label receiver;
+    public Label transaction_amount;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
