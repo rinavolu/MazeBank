02ca6e9a82e685834ebd5d9dd143bed1e849a1b0
Ravindranadh Inavolu <ravindranadh.inavolu@gmail.com>
2024-02-23 10:46:07 +0530

    Created ClientLisView cell designs @Admin pane

---
+++ src/main/java/com/bank/maze/mazebank/Controllers/Admin/ClientCellController.java
@@ -0,0 +1,23 @@
+package com.bank.maze.mazebank.Controllers.Admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientCellController implements Initializable {
+    public Label firstName;
+    public Label lastName;
+    public Label pAddress;
+    public Label checkingAccountNumber;
+    public Label savingsAccountNumber;
+    public Label AccountCreatedDate;
+    public Button userDeleteButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
